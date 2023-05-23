c4b6e222f1f39e54681835770a84e256c142f3f4
RIPU DAMAN <ripudaman600@gmail.com>
2023-05-23 15:10:19 +1000

    Initial Commit - Nail Health

---
+++ Home.py
@@ -0,0 +1,63 @@
+import streamlit as st
+
+st.set_page_config(page_title = 'Nail Health', 
+                    layout='wide',
+                    page_icon='./images/nail.png')
+st.subheader('_Yolo for nail health_')
+st.balloons()
+# About
+st.markdown("""
+            #### This project aims to provide classification results on the different types of nail diseases. Overall, we have compiled a list of 11 nail diseases as per the WHO approved clinical diseases for nails. They are as follows;  
+            """)
+
+st.markdown(
+    """
+    <style>
+    .stTabs > div {
+        overflow: auto !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+img_value_size = 200
+st.subheader("Nail Diseases :-")
+
+with st.container():
+    disease1, disease2, disease3, disease4, disease5 = st.tabs(['Acral Lentiginous Melanoma', 'Beaus Line', 'Blue Finger', 'Clubbing', 'Healthy Nail'])
+
+    with disease1:
+        st.image('./images/Acral Lentiginous Melanoma.jpg', width=img_value_size)
+
+    with disease2:
+        st.image('./images/Beaus Line.jpg', width=img_value_size)
+
+    with disease3:
+        st.image('./images/Blue Finger.jpg', width=img_value_size)
+
+    with disease4:
+        st.image('./images/Clubbing.jpg', width=img_value_size)
+
+    with disease5:
+        st.image('./images/Healthy Nail.jpg', width=img_value_size)
+
+with st.container():
+    disease6, disease7, disease8, disease9, disease10, disease11 = st.tabs(['Koilonychia', 'Lindsay-s Nail', 'Muehrckes Lines', 'Onychogryphosis', 'Pitting', 'Terry-s Nail'])
+
+    with disease6:
+        st.image('./images/Koilonychia.jpg', width=img_value_size)
+
+    with disease7:
+        st.image('./images/Lindsay-s Nail.jpg', width=img_value_size)
+
+    with disease8:
+        st.image('./images/Muehrckes Lines.jpg', width=img_value_size)
+
+    with disease9:
+        st.image('./images/Onychogryphosis.jpg', width=img_value_size)
+
+    with disease10:
+        st.image('./images/pitting.jpg', width=img_value_size)
+
+    with disease11:
+        st.image('./images/Terry-s Nail.jpg', width=img_value_size)
---
+++ pages/Check_Your_Nails.py
@@ -0,0 +1,195 @@
+# import streamlit as st
+# from yolo_predictions import YOLO_Pred
+# from PIL import Image
+# import numpy as np
+
+# st.set_page_config(page_title="YOLO Object Detection",
+#                    layout='wide',
+#                    page_icon='./images/nail.png')
+
+# st.header('Welcome to Nail Health Portal')
+# # st.write('Please Upload Image to get detections')
+
+# with st.spinner('Please wait while your model is loading'):
+#     yolo = YOLO_Pred(onnx_model=r'C:/Users/ripud/Desktop/OneDrive - UTS/UTS/Sem 3/DEEP_LEARNING/GUI/Ripu/Nail_Health/models/nail_yolo/best.onnx',
+#                     data_yaml=r'C:/Users/ripud/Desktop/OneDrive - UTS/UTS/Sem 3/DEEP_LEARNING/GUI/Ripu/Nail_Health/models/nail_yolo/data.yaml')
+                    
+
+
+
+# col1, col2 = st.columns(2)
+# with col1:
+#     # Upload Image
+#     image_file = st.file_uploader(label='Upload Image')
+#     def upload_image():
+#         if image_file is not None:
+#             size_mb = image_file.size/(1024**2)
+#             file_details = {"filename":image_file.name,
+#                             "filetype":image_file.type,
+#                             "filesize": "{:,.2f} MB".format(size_mb)}
+#             #st.json(file_details)
+#             # validate file
+#             if file_details['filetype'] in ('image/png','image/jpeg'):
+#                 st.success('VALID IMAGE file type (png or jpeg)')
+#                 return {"file":image_file,
+#                         "details":file_details}
+            
+#             else:
+#                 st.error('INVALID Image file type')
+#                 st.error('Upload only png,jpg, jpeg')
+#                 return None
+
+# with col2:
+#     st.markdown("##### Take Picture #####")
+#     buttonVal = st.button('Do you want to take picture from Camera?')
+#     def camera_input():
+#         if buttonVal:
+#             picture = st.camera_input("Take a picture")
+#             if picture:
+#                 st.image(picture)
+                
+        
+# def main():
+#     object = upload_image()
+#     object1 = camera_input()
+#     if object:
+#         prediction = False
+#         image_obj = Image.open(object['file'])       
+        
+#         col1 , col2 = st.columns(2)
+        
+#         with col1:
+#             st.info('Preview of Image')
+#             st.image(image_obj)
+            
+#         with col2:
+#             st.subheader('Check below for file details')
+#             st.json(object['details'])
+#             button = st.button('Get Detection from YOLO')
+#             if button:
+#                 with st.spinner("""
+#                 Geting Objects from image. please wait
+#                                 """):
+#                     # below command will convert
+#                     # obj to array
+#                     image_array = np.array(image_obj)
+#                     pred_img = yolo.predictions(image_array)
+#                     pred_img_obj = Image.fromarray(pred_img)
+#                     prediction = True
+                
+#         if prediction:
+#             st.subheader("Predicted Image")
+#             st.caption("Object detection from YOLO V5 model")
+#             st.image(pred_img_obj)
+    
+#     # elif object1:
+#     #     st.image(object1['Picture'])
+#         # prediction = False
+#         # image_obj = Image.open(object['file'])       
+        
+#         # col1 , col2 = st.columns(2)
+        
+#         # with col1:
+#         #     st.info('Preview of Image')
+#         #     st.image(image_obj)
+            
+#         # with col2:
+#         #     st.subheader('Check below for file details')
+#         #     st.json(object['details'])
+#         #     button = st.button('Get Detection from YOLO')
+#         #     if button:
+#         #         with st.spinner("""
+#         #         Geting Objets from image. please wait
+#         #                         """):
+#         #             # below command will convert
+#         #             # obj to array
+#         #             image_array = np.array(image_obj)
+#         #             pred_img = yolo.predictions(image_array)
+#         #             pred_img_obj = Image.fromarray(pred_img)
+#         #             prediction = True
+                
+#         # if prediction:
+#         #     st.subheader("Predicted Image")
+#         #     st.caption("Object detection from YOLO V5 model")
+#         #     st.image(pred_img_obj)
+    
+    
+    
+# if __name__ == "__main__":
+#     main()
+
+import streamlit as st
+from yolo_predictions import YOLO_Pred
+from PIL import Image
+import numpy as np
+
+st.set_page_config(page_title="YOLO Object Detection",
+                   layout='wide',
+                   page_icon='./images/nail.png')
+
+st.header('Get Object Detection for any Image')
+st.write('Please Upload Image to get detections')
+
+with st.spinner('Please wait while your model is loading'):
+    yolo = YOLO_Pred(onnx_model=r'./models/nail_yolo/best.onnx',
+                    data_yaml=r'./models/nail_yolo/data_final.yml')
+    #st.balloons()
+
+def upload_image():
+    # Upload Image
+    image_file = st.file_uploader(label='Upload Image')
+    if image_file is not None:
+        size_mb = image_file.size/(1024**2)
+        file_details = {"filename":image_file.name,
+                        "filetype":image_file.type,
+                        "filesize": "{:,.2f} MB".format(size_mb)}
+        #st.json(file_details)
+        # validate file
+        if file_details['filetype'] in ('image/png','image/jpeg'):
+            st.success('VALID IMAGE file type (png or jpeg')
+            return {"file":image_file,
+                    "details":file_details}
+        
+        else:
+            st.error('INVALID Image file type')
+            st.error('Upload only png,jpg, jpeg')
+            return None
+        
+def main():
+    object = upload_image()
+    
+    if object:
+        prediction = False
+        image_obj = Image.open(object['file'])       
+        
+        col1 , col2 = st.columns(2)
+        
+        with col1:
+            st.info('Preview of Image')
+            st.image(image_obj)
+            
+        with col2:
+            st.subheader('Check below for file details')
+            st.json(object['details'])
+            button = st.button('Get Detection from YOLO')
+            
+            if button:
+                with st.spinner("""
+                Geting Objets from image. please wait
+                                """):
+                    # below command will convert
+                    # obj to array
+                    image_array = np.array(image_obj)
+                    pred_img = yolo.predictions(image_array)
+                    pred_img_obj = Image.fromarray(pred_img)
+                    prediction = True
+                
+        if prediction:
+            st.subheader("Predicted Image")
+            st.caption("Object detection from YOLO V5 model")
+            st.image(pred_img_obj)
+    
+    
+    
+if __name__ == "__main__":
+    main()
